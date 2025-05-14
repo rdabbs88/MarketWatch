9e415114588ac6ab625c863791168b8deaef19a8
Ryan Dabbs <rdabbs88@umd.edu>
2025-05-14 13:37:38 -0400

    Fixing vercel.json and getting rid of unecessary api folder

--- api/index.py
+++
@@ -1,9 +0,0 @@
-import os
-from flask_app import create_app
-
-os.environ["MPLCONFIGDIR"] = "/tmp"
-
-app = create_app()
-
-def handler(environ, start_response):
-    return app.wsgi_app(environ, start_response)
